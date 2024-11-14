115475fdd7b87ac3562e220b781d04fa59bae767
Sőrés Gábor <gabor.sores86@gmail.com>
2024-11-14 10:31:14 +0100

    feature: create task table with new models and factories/seeders

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,22 @@
+<?php
+namespace Database\Factories;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    protected $model = Task::class;
+
+    public function definition()
+    {
+        return [
+            'user_id' => $this->faker->randomElement([null, User::inRandomOrder()->first()?->id]),
+            'description' => $this->faker->sentence,
+            'estimated_time' => $this->faker->numberBetween(1000, 10000),
+            'used_time' => $this->faker->numberBetween(10, 10000),
+            'completed_at' => $this->faker->optional()->dateTime,
+        ];
+    }
+}
---
+++ database/migrations/2024_11_14_090037_create_tasks.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null');
+            $table->tinyText('description');
+            $table->integer('estimated_time');
+            $table->integer('used_time')->nullable();
+            $table->timestamps();
+            $table->timestamp('completed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
