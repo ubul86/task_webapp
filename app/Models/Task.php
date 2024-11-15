00678f4567b8c63048f94c24d1e6a77e8b65d77e
Sőrés Gábor <gabor.sores86@gmail.com>
2024-11-15 18:45:40 +0100

    feature: cast datetimes and add accessors to task model to format datetimes

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -13,6 +13,10 @@ class Task extends Model
 
     protected $table = 'tasks';
 
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
     protected $fillable = ['description', 'completed_at', 'used_time', 'estimated_time', 'user_id'];
 
     /** @return BelongsTo<User, Task> */
@@ -27,5 +31,19 @@ class Task extends Model
         $this->save();
 
         return $this;
+    }
+
+    public function getCreatedAtAttribute(?string $value): ?string
+    {
+        return $value
+            ? Carbon::parse($value)->format('Y-m-d H:i:s')
+            : null;
+    }
+
+    public function getUpdatedAtAttribute(?string $value): ?string
+    {
+        return $value
+            ? Carbon::parse($value)->format('Y-m-d H:i:s')
+            : null;
     }
 }
