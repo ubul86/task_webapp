66e389786f27cbddce0d44d9286df4bf0d932a73
Sőrés Gábor <gabor.sores86@gmail.com>
2024-11-15 18:14:14 +0100

    feature: set route to fetch all users

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 
 Route::put('/tasks/set-completed/{id}', [TaskController::class, 'setCompleted']);
 Route::put('/tasks/set-bulk-completed', [TaskController::class, 'setBulkCompleted']);
 Route::delete('/tasks/bulk-destroy', [TaskController::class, 'bulkDestroy']);
 
 Route::apiResource('tasks', TaskController::class);
+Route::apiResource('users', UserController::class);
 
